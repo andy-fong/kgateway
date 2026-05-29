310fd7fdb01b302e1fe5bce99e83a76325a2261d
Andy Fong <andy.fong@solo.io>
2026-05-29 18:27:46 -0400

    added http listener tests
    Signed-off-by: Andy Fong <andy.fong@solo.io>

--- pkg/kgateway/translator/gateway/gateway_translator_test.go
+++ pkg/kgateway/translator/gateway/gateway_translator_test.go
@@ -1294,6 +1294,17 @@ func TestBasic(t *testing.T) {
 		})
 	})
 
+	t.Run("TrafficPolicy retry targeting HTTP listener", func(t *testing.T) {
+		test(t, translatorTestCase{
+			inputFile:  "traffic-policy/retry-http-listener.yaml",
+			outputFile: "traffic-policy/retry-http-listener.yaml",
+			gwNN: types.NamespacedName{
+				Namespace: "default",
+				Name:      "example-gateway",
+			},
+		})
+	})
+
 	t.Run("TrafficPolicy timeout attached to GRPCRoute", func(t *testing.T) {
 		test(t, translatorTestCase{
 			inputFile:  "traffic-policy/grpcroute-timeout.yaml",
